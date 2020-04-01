ba29ed0409e035168ada50c2d0fe1cffef42e03c
Nemanja Petrovic <nemanja.petrovic79@gmail.com>
2020-04-01 14:37:53 +0200

    Initial Commit

---
+++ WebCamMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebCamMap
+//
+//  Created by Nemanja Petrovic on 01/04/2020.
+//  Copyright © 2020 Nemanja Petrovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
