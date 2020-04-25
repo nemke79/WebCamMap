837f9b3d9eccdce32969b4d6373417682db84704
Nemanja Petrovic <nemanja.petrovic79@gmail.com>
2020-04-25 16:27:28 +0200

    Removed moveRow when adding/removing location to/from favourites, to avoid confusing users with animations.

--- WebCamMap/FavouriteCities.swift
+++ WebCamMap/FavouriteCities.swift
@@ -12,20 +12,32 @@ import CoreData
 class FavouriteCities: NSManagedObject {
     
     class func addFavouriteCity(name: String, latitude: Double, longitude: Double, context: NSManagedObjectContext) {
-        let favouriteCity = FavouriteCities(context: context)
-        favouriteCity.cityName = name
-        favouriteCity.latitude = latitude
-        favouriteCity.longitude = longitude
-        favouriteCity.dateCreated = Date()
-    }
-    
-    class func deleteFavouriteCity(matching item: Int, into context: NSManagedObjectContext ) {
         let request: NSFetchRequest<FavouriteCities> = FavouriteCities.fetchRequest()
+        
+        //In predicate we use long float - lf for compare because otherwise values that are compared never will be the same, because float and double precision is not 100%.
+        request.predicate = NSPredicate(format: "latitude == %lf AND longitude == %lf", latitude, longitude)
         request.sortDescriptors = [NSSortDescriptor(key: "dateCreated", ascending: true)]
         let favouriteCity = try? context.fetch(request)
         
+        if favouriteCity!.count == 0 {
+            let favCity = FavouriteCities(context: context)
+            favCity.cityName = name
+            favCity.latitude = latitude
+            favCity.longitude = longitude
+            favCity.dateCreated = Date()
+        }
+    }
+    
+    class func deleteFavouriteCity(latitude: Double, longitude: Double, into context: NSManagedObjectContext ) {
+        let request: NSFetchRequest<FavouriteCities> = FavouriteCities.fetchRequest()
+        
+        //In predicate we use long float - lf for compare because otherwise values that are compared never will be the same, because float and double precision is not 100%.
+        request.predicate = NSPredicate(format: "latitude == %lf AND longitude == %lf", latitude, longitude)
+        request.sortDescriptors = [NSSortDescriptor(key: "dateCreated", ascending: true)]
+        let favouriteCity = try? context.fetch(request)
+
         if favouriteCity!.count > 0 {
-            context.delete(favouriteCity![item])
+            context.delete(favouriteCity!.first!)
         }
     }
 }
--- WebCamMap/WebCamMapViewController.swift
+++ WebCamMap/WebCamMapViewController.swift
@@ -35,7 +35,7 @@ class WebCamMapViewController: UIViewController, UITableViewDelegate, UITableVie
     
     var webCamInfo = WebCamInfo()
     
-    var counter = 0
+ //   var counter = 0
     
     @IBAction func searchButton(_ sender: Any) {
      let searchController = UISearchController()
@@ -256,13 +256,13 @@ class WebCamMapViewController: UIViewController, UITableViewDelegate, UITableVie
             
             cityNameCell.actionBlock = {
                 if cityNameCell.favouriteButton.currentImage! == UIImage(systemName: "video.circle"){
-                    for value in self.arrayOfFavourites {
-                        if value == true {
-                            self.counter += 1
-                        } else {
-                            break
-                        }
-                    }
+//                    for value in self.arrayOfFavourites {
+//                        if value == true {
+//                            self.counter += 1
+//                        } else {
+//                            break
+//                        }
+//                    }
                     cityNameCell.favouriteButton.setImage(UIImage(systemName: "video.circle.fill"), for: .normal)
                     FavouriteCities.addFavouriteCity(name: cityNameCell.cityName.text!, latitude: self.arrayOfAnotations[indexPath.row].coordinate.latitude, longitude: self.arrayOfAnotations[indexPath.row].coordinate.longitude, context: self.context)
                     self.arrayOfFavourites[indexPath.row] = true
@@ -271,52 +271,52 @@ class WebCamMapViewController: UIViewController, UITableViewDelegate, UITableVie
                     
                     // Move row to the top of tableview when its added to favourites, below other favourites. Otherwise, remove it from favourites and put it below favourites.
                     
-                    self.citiesTableView.beginUpdates()
-                    
-                    self.citiesTableView.moveRow(at: indexPath, to: IndexPath(row: self.counter, section: 0))
-                    
-                    self.citiesTableView.endUpdates()
-                    
-                    self.arrayOfFavourites.insert(self.arrayOfFavourites.remove(at: indexPath.row), at: self.counter)
-                    self.arrayOfPinsNames.insert(self.arrayOfPinsNames.remove(at: indexPath.row), at: self.counter)
-                    self.arrayOfAnotations.insert(self.arrayOfAnotations.remove(at: indexPath.row), at: self.counter)
-                    self.arrayOfPinsCLLocations.insert(self.arrayOfPinsCLLocations.remove(at: indexPath.row), at: self.counter)
-                    
-                    self.citiesTableView.reloadData()
-                    
-                    let indxPath = NSIndexPath(row: self.counter, section: 0)
-                    
-                    self.citiesTableView.scrollToRow(at: indxPath as IndexPath, at: .top, animated: true)
-                    
-                    self.counter = 0
+//                    self.citiesTableView.beginUpdates()
+//
+//                    self.citiesTableView.moveRow(at: indexPath, to: IndexPath(row: self.counter, section: 0))
+//
+//                    self.citiesTableView.endUpdates()
+//
+//                    self.arrayOfFavourites.insert(self.arrayOfFavourites.remove(at: indexPath.row), at: self.counter)
+//                    self.arrayOfPinsNames.insert(self.arrayOfPinsNames.remove(at: indexPath.row), at: self.counter)
+//                    self.arrayOfAnotations.insert(self.arrayOfAnotations.remove(at: indexPath.row), at: self.counter)
+//                    self.arrayOfPinsCLLocations.insert(self.arrayOfPinsCLLocations.remove(at: indexPath.row), at: self.counter)
+//
+//                    self.citiesTableView.reloadData()
+//
+//                    let indxPath = NSIndexPath(row: self.counter, section: 0)
+//
+//                    self.citiesTableView.scrollToRow(at: indxPath as IndexPath, at: .top, animated: true)
+//
+//                    self.counter = 0
                     
                 } else if cityNameCell.favouriteButton.currentImage! == UIImage(systemName: "video.circle.fill") {
-                    for value in self.arrayOfFavourites {
-                        if value == true {
-                            self.counter += 1
-                        } else {
-                            break
-                        }
-                    }
+//                    for value in self.arrayOfFavourites {
+//                        if value == true {
+//                            self.counter += 1
+//                        } else {
+//                            break
+//                        }
+//                    }
                     cityNameCell.favouriteButton.setImage(UIImage(systemName: "video.circle"), for: .normal)
-                    FavouriteCities.deleteFavouriteCity(matching: indexPath.row, into: self.context)
+                    FavouriteCities.deleteFavouriteCity(latitude: self.arrayOfAnotations[indexPath.row].coordinate.latitude, longitude: self.arrayOfAnotations[indexPath.row].coordinate.longitude, into: self.context)
                     self.arrayOfFavourites[indexPath.row] = false
                     try? self.context.save()
                     
-                    self.citiesTableView.beginUpdates()
-                    
-                    self.citiesTableView.moveRow(at: indexPath, to: IndexPath(row: self.counter - 1, section: 0))
-                    
-                    self.citiesTableView.endUpdates()
-                    
-                    self.arrayOfFavourites.insert(self.arrayOfFavourites.remove(at: indexPath.row), at: self.counter - 1)
-                    self.arrayOfPinsNames.insert(self.arrayOfPinsNames.remove(at: indexPath.row), at: self.counter - 1)
-                    self.arrayOfAnotations.insert(self.arrayOfAnotations.remove(at: indexPath.row), at: self.counter - 1)
-                    self.arrayOfPinsCLLocations.insert(self.arrayOfPinsCLLocations.remove(at: indexPath.row), at: self.counter - 1)
-                    
-                    self.citiesTableView.reloadData()
-                    
-                    self.counter = 0
+//                    self.citiesTableView.beginUpdates()
+//
+//                    self.citiesTableView.moveRow(at: indexPath, to: IndexPath(row: self.counter - 1, section: 0))
+//
+//                    self.citiesTableView.endUpdates()
+//
+//                    self.arrayOfFavourites.insert(self.arrayOfFavourites.remove(at: indexPath.row), at: self.counter - 1)
+//                    self.arrayOfPinsNames.insert(self.arrayOfPinsNames.remove(at: indexPath.row), at: self.counter - 1)
+//                    self.arrayOfAnotations.insert(self.arrayOfAnotations.remove(at: indexPath.row), at: self.counter - 1)
+//                    self.arrayOfPinsCLLocations.insert(self.arrayOfPinsCLLocations.remove(at: indexPath.row), at: self.counter - 1)
+//
+//                    self.citiesTableView.reloadData()
+//
+//                    self.counter = 0
                 }
             }
         }
@@ -326,14 +326,14 @@ class WebCamMapViewController: UIViewController, UITableViewDelegate, UITableVie
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            if arrayOfFavourites[indexPath.row] == true {
+                FavouriteCities.deleteFavouriteCity(latitude: arrayOfAnotations[indexPath.row].coordinate.latitude, longitude: arrayOfAnotations[indexPath.row].coordinate.longitude, into: self.context)
+                try? self.context.save()
+            }
             mapView.removeAnnotation(arrayOfAnotations[indexPath.row])
             arrayOfAnotations.remove(at: indexPath.row)
             arrayOfPinsNames.remove(at: indexPath.row)
             arrayOfPinsCLLocations.remove(at: indexPath.row)
-            if arrayOfFavourites[indexPath.row] == true {
-                FavouriteCities.deleteFavouriteCity(matching: indexPath.row, into: self.context)
-                try? self.context.save()
-            }
             arrayOfFavourites.remove(at: indexPath.row)
             citiesTableView.deleteRows(at: [indexPath], with: .fade)
             citiesTableView.reloadData()
